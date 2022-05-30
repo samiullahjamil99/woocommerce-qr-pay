38afe6542b321104a03e7b8ccbd72fa46baa59a4
samiullahjamil99 <jamil.samiullah@gmail.com>
2022-05-30 10:05:14 +0500

    Basic Plugin

---
+++ woocommerce-qr-pay.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package Woocommerce_QR_Pay
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Woocommerce QR Pay
+Author: Samiullah Jamil
+Version: 1.0.0
+Author URI: https://www.samiullahjaml.com/about-me/
+*/
